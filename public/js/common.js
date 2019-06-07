fb079da8c15a361a966d55da01964f00d46d29cc
Pavel <wol1414@gmail.com>
2019-06-07 19:17:51 +0300

    edit common.js

--- public/js/common.js
+++ public/js/common.js
@@ -1,63 +1,221 @@
 var btnToggle = $(".toggle-menu-mobile--js")
-menu = $(".menu-mobile--js")
-JSCCommon = {
-	// часть вызов скриптов здесь, для использования при AJX
-	LazyFunction: function () {
-		// Для лэзи загрузки 
+		menu = $(".menu-mobile--js")
 
-		document.addEventListener("DOMContentLoaded", function () {
-			let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-			let active = false;
+jQuery(document).ready(function ($) {
 
-			const lazyLoad = function () {
-				if (active === false) {
-					active = true;
+	// для свг
+	svg4everybody({}); 
+	JSCCommon.magnificPopupCall();
 
-					setTimeout(function () {
-						lazyImages.forEach(function (lazyImage) {
-							if (((lazyImage.getBoundingClientRect().top - lazyImage.parentElement.clientHeight * 2) <= window.innerHeight && (lazyImage.getBoundingClientRect().bottom + lazyImage.parentElement.clientHeight * 2) >= 0) && getComputedStyle(lazyImage).display !== "none") {
-								lazyImage.src = lazyImage.dataset.src;
-								// lazyImage.srcset = lazyImage.dataset.srcset;
-								lazyImage.classList.remove("lazy").classList.add("lazyloaded");
+	JSCCommon.tabscostume('tabs');
 
-								lazyImages = lazyImages.filter(function (image) {
-									return image !== lazyImage;
-								});
+	JSCCommon.mobileMenu();
 
-								if (lazyImages.length === 0) {
-									document.removeEventListener("scroll", lazyLoad);
-									window.removeEventListener("resize", lazyLoad);
-									window.removeEventListener("orientationchange", lazyLoad);
-									window.addEventListener("DOMContentLoaded", lazyLoad);
-								}
-							}
-						});
+	JSCCommon.inputMask();
 
-						active = false;
-					}, 200);
-				}
-			};
+	JSCCommon.inlineSVG();
 
-			document.addEventListener("scroll", lazyLoad);
-			window.addEventListener("resize", lazyLoad);
-			window.addEventListener("orientationchange", lazyLoad);
-			window.addEventListener("DOMContentLoaded", lazyLoad);
+	JSCCommon.CustomInputFile();
+
+	JSCCommon.CustomYoutubeBlock();
+
+
+
+	
+	var url = document.location.href;
+	$.each($(".top-nav__nav a "), function () {
+
+		if (this.href == url) {
+			if ($(this).hasClass("top-nav__link") == true) {
+
+				$(this).addClass('top-nav__link-active');
+			}
+			if ($(this).hasClass("footer__link") == true) {
+
+				$(this).addClass('footer__link-active');
+			} 
+		}; 
+	}); 
+	// / закрыть меню при горизонтальном свайпе
+	// /закрыть/открыть мобильное меню
+
+	function heightses() {
+
+		var w = $(window).width();
+
+		// $(".main-wrapper").css("margin-bottom", $('footer').height())
+		// $(".otz__item .text-wrap ").height('auto').equalHeights();
+		// 
+		// скрывает моб меню
+
+		var topH = $("header ").innerHeight();
+
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > topH) {
+				$('.top-nav  ').addClass('fixed');
+			} else {
+				$('.top-nav  ').removeClass('fixed');
+			}
 		});
+		// конец добавил
+		if (window.matchMedia("(min-width: 992px)").matches) {
+
+			btnToggle.removeClass("on");
+			// $("body").removeClass("fixed");
+			menu.removeClass("active");
+			$("body").removeClass("fixed");
+		}
+	} 
+
+	$(window).resize(function () {
+		heightses();
+
+	});
+	$(window).on("load", function () {
+		heightses();
+
+	})
+
+	heightses();
+ 
+	// листалка по стр
+	$(" .top-nav, .scroll-link").click(function () {
+	       var elementClick = $(this).attr("href");
+	       var destination = $(elementClick).offset().top;
+
+	           $('html, body').animate({ scrollTop: destination }, 1100);
+
+	       return false;
+	   }); 
+
+	// var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
+
+	// var arrl2 = (' <div class="r">' + icon),
+	// 	arrr2 = (' <div class="l">' + icon);
+	// // карусель
+	// $('.s-team__slider').slick({
+	// 	slidesToShow: 3,
+	// 	slidesToScroll: 1,
+	// 	dots: false,
+	// 	speed: 900,
+	// 	infinite: true,
+	// 	loop: true,
+	// 	arrows: true,
+	// 	mobileFirst: true,
+	// 	// centerMode: true,
+	// 	// focusOnSelect: true ,
+	// 	// variableWidth: true,
+	// 	prevArrow: arrr2,
+	// 	nextArrow: arrl2,
+	// });
+
+	$('.s-gal__slider\
+	,.slider-for2 ')
+		.on('lazyLoaded', function (event, slick, image, imageSource) {
+			image.parent().css('background-image', 'url(' + image.attr('src') + ')');
+		});
+	// slider
+	// var swiper4 = new Swiper('.color-slider', {
+	// 	// slidesPerView: 5,
+	// 	slidesPerView: 'auto',
+	// 	watchOverflow: true,
+	// 	spaceBetween: 0,
+	// 	freeMode: true,
+	// 	watchOverflow: true,
+	// 	slidesPerGroup: 3,
+
+	// 	// centeredSlides: true,
+	// 	loop: true,
+	// 	loopFillGroupWithBlank: true,
+	// 	touchRatio: 0.2,
+	// 	slideToClickedSlide: true,
+	// 	freeModeMomentum: true,
+	// 	navigation: {
+	// 		nextEl: '.swiper-button-next',
+	// 		prevEl: '.swiper-button-prev',
+	// 	},
+
+	// });
+	// modal window
 
 
+	// form
+	$("form").submit(function () { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: 'action.php', //Change
+			data: th.serialize()
+		}).success(function () {
+			$.magnificPopup.close();
+			$.magnificPopup.open({
+				items: {
+					src: '#thanks', // can be a HTML string, jQuery object, or CSS selector
+					type: 'inline'
+				}
+			})
+			// window.location.replace("/thanks.html");
+			setTimeout(function () {
+				// Done Functions
+				th.trigger("reset");
+				// $.magnificPopup.close();
+			}, 4000);
+		});
+		return false;
+	});
+	// /form
+ 
+	// или
+	// $(".dropzone").dropzone({
+	//  url: "/file/post",
+	//  addRemoveLinks: true,
+	//      acceptedFiles: 'image/*',
+	//      uploadMultiple: true,
+	//   });
+
+
+
+
+	// $(".wow-wrap").each(function () {
+	// var wowAnim = $(this).find(".s-dop__col," +
+	//                 ".s-pick__col," +
+	//                 ".s-condition__col");
+	// wowAnim.each(function(i){
+
+	// wowAnim.eq(i).attr("data-wow-delay", i*.1*2 + "s");
+
+	//    var wow = new WOW({ mobile: false });
+	//         wow.init();
+
+	// });
+	// });
+
+
+});
+JSCCommon = {
+	// часть вызов скриптов здесь, для использования при AJAX
+	// функции для запуска lazy
+	lazyShow: function (lazyLoad) {
+		document.removeEventListener("scroll", lazyLoad);
+		window.removeEventListener("resize", lazyLoad);
+		window.removeEventListener("orientationchange", lazyLoad);
+		window.addEventListener("DOMContentLoaded", lazyLoad);
+	},
+	LazyFunction: function () {
 		// лэзи 
 		document.addEventListener("DOMContentLoaded", function () {
-			let lazyImages = [].slice.call(document.querySelectorAll(".lazy-bg"));
-			let active = false;
+			var lazyBg = [].slice.call(document.querySelectorAll(".lazy-bg"));
+			var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+			var active = false;
 
-			const lazyLoad = function () {
+
+			function lazyLoad() {
 				if (active === false) {
 					active = true;
-
 					setTimeout(function () {
+
 						lazyImages.forEach(function (lazyImage) {
-							if (((lazyImage.getBoundingClientRect().top - lazyImage.parentElement.clientHeight) <= window.innerHeight && (lazyImage.getBoundingClientRect().bottom + lazyImage.parentElement.clientHeight) >= 0) && getComputedStyle(lazyImage).display !== "none") {
-								lazyImage.parentElement.style.backgroundImage = 'url(' + lazyImage.dataset.src + ')';
+							if (((lazyImage.getBoundingClientRect().top - lazyImage.closest(".block-with-lazy").clientHeight * 2) <= window.innerHeight && (lazyImage.getBoundingClientRect().bottom + lazyImage.closest(".block-with-lazy").clientHeight * 2) >= 0) && getComputedStyle(lazyImage).display !== "none") {
 								lazyImage.src = lazyImage.dataset.src;
 								// lazyImage.srcset = lazyImage.dataset.srcset;
 								lazyImage.classList.remove("lazy");
@@ -65,28 +223,35 @@ JSCCommon = {
 								lazyImages = lazyImages.filter(function (image) {
 									return image !== lazyImage;
 								});
-
 								if (lazyImages.length === 0) {
-									document.removeEventListener("scroll", lazyLoad);
-									window.removeEventListener("resize", lazyLoad);
-									window.removeEventListener("orientationchange", lazyLoad);
-									window.addEventListener("DOMContentLoaded", lazyLoad);
+									JSCCommon.lazyShow(lazyLoad)
 								}
 							}
 						});
+						lazyBg.forEach(function (lazyImage) {
+							if (((lazyImage.getBoundingClientRect().top - lazyImage.closest(".block-with-lazy").clientHeight * 2) <= window.innerHeight && (lazyImage.getBoundingClientRect().bottom + lazyImage.closest(".block-with-lazy").clientHeight * 2) >= 0) && getComputedStyle(lazyImage).display !== "none") {
+								lazyImage.parentElement.style.backgroundImage = 'url(' + lazyImage.dataset.src + ')';
+								lazyImage.src = lazyImage.dataset.src;
+								lazyImage.classList.remove("lazy");
+								lazyBg = lazyBg.filter(function (image) {
+									return image !== lazyImage;
+								});
 
+								if (lazyBg.length === 0) {
+									JSCCommon.lazyShow(lazyLoad)
+								}
+							}
+						});
 						active = false;
 					}, 200);
 				}
 			};
 
-			document.addEventListener("scroll", lazyLoad);
-			window.addEventListener("resize", lazyLoad);
-			window.addEventListener("orientationchange", lazyLoad);
-			window.addEventListener("DOMContentLoaded", lazyLoad);
+			JSCCommon.lazyShow(lazyLoad)
 		});
 
 	},
+
 
 	// /LazyFunction
 
@@ -175,7 +340,7 @@ JSCCommon = {
 			triggerOnTouchEnd: false,
 		});
 	},
-// /mobileMenu
+	// /mobileMenu
 
 	// табы  . 
 	tabscostume: function (tab) {
@@ -187,7 +352,7 @@ JSCCommon = {
 
 		});
 	},
-// /табы  . 
+	// /табы  . 
 
 
 
@@ -258,226 +423,3 @@ JSCCommon = {
 JSCCommon.LazyFunction();
 /***/
 
-jQuery(document).ready(function ($) {
-
-	// для свг
-	svg4everybody({});
-	// Custom JS
-
-	// вызов magnificPopupCall
-	JSCCommon.magnificPopupCall();
-
-	JSCCommon.tabscostume('tabs');
-
-	JSCCommon.mobileMenu();
-
-	JSCCommon.inputMask();
-
-	JSCCommon.inlineSVG();
-
-	JSCCommon.CustomInputFile();
-
-	JSCCommon.CustomYoutubeBlock();
-
-
-
-
-	var url = document.location.href;
-	$.each($(".top-nav__nav a "), function () {
-
-		if (this.href == url) {
-			if ($(this).hasClass("top-nav__link") == true) {
-
-				$(this).addClass('top-nav__link-active');
-			}
-			if ($(this).hasClass("footer__link") == true) {
-
-				$(this).addClass('footer__link-active');
-			}
-
-		};
-
-	});
-
-
-	// / закрыть меню при горизонтальном свайпе
-	// /закрыть/открыть мобильное меню
-
-	function heightses() {
-
-		var w = $(window).width();
-
-		// $(".main-wrapper").css("margin-bottom", $('footer').height())
-		// $(".otz__item .text-wrap ").height('auto').equalHeights();
-		//
-
-
-		// скрывает моб меню
-
-		var topH = $("header ").innerHeight();
-
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > topH) {
-				$('.top-nav  ').addClass('fixed');
-			} else {
-				$('.top-nav  ').removeClass('fixed');
-			}
-		});
-		// конец добавил
-		if (window.matchMedia("(min-width: 992px)").matches) {
-
-			btnToggle.removeClass("on");
-			// $("body").removeClass("fixed");
-			menu.removeClass("active");
-			$("body").removeClass("fixed");
-		}
-	}
-
-
-
-	$(window).resize(function () {
-		heightses();
-
-	});
-	$(window).on("load", function () {
-		heightses();
-
-	})
-
-	heightses();
-
-
-
-	// листалка по стр
-	// $(" .top-nav a").click(function () {
-	//        var elementClick = $(this).attr("href");
-	//        var destination = $(elementClick).offset().top;
-
-	//            $('html, body').animate({ scrollTop: destination }, 1100);
-
-	//        return false;
-	//    });
-
-
-
-
-
-
-
-	var icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
-
-	var arrl2 = (' <div class="r">' + icon),
-		arrr2 = (' <div class="l">' + icon);
-	// карусель
-	$('.s-team__slider').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		dots: false,
-		speed: 900,
-		infinite: true,
-		loop: true,
-		arrows: true,
-		mobileFirst: true,
-		// centerMode: true,
-		// focusOnSelect: true ,
-		// variableWidth: true,
-		prevArrow: arrr2,
-		nextArrow: arrl2,
-	});
-
-	$('.s-gal__slider,' +
-			' .s-project__slider--js ,' +
-			' .slider-for ,' +
-			' .slider-for2 ')
-		.on('lazyLoaded', function (event, slick, image, imageSource) {
-			image.parent().css('background-image', 'url(' + image.attr('src') + ')');
-		});
-	// slider
-	// var swiper4 = new Swiper('.color-slider', {
-	// 	// slidesPerView: 5,
-	// 	slidesPerView: 'auto',
-	// 	watchOverflow: true,
-	// 	spaceBetween: 0,
-	// 	freeMode: true,
-	// 	watchOverflow: true,
-	// 	slidesPerGroup: 3,
-
-	// 	// centeredSlides: true,
-	// 	loop: true,
-	// 	loopFillGroupWithBlank: true,
-	// 	touchRatio: 0.2,
-	// 	slideToClickedSlide: true,
-	// 	freeModeMomentum: true,
-	// 	navigation: {
-	// 		nextEl: '.swiper-button-next',
-	// 		prevEl: '.swiper-button-prev',
-	// 	},
-
-	// });
-	// modal window
-
-
-	// form
-	$("form").submit(function () { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: 'action.php', //Change
-			data: th.serialize()
-		}).success(function () {
-			$.magnificPopup.close();
-			$.magnificPopup.open({
-				items: {
-					src: '#thanks', // can be a HTML string, jQuery object, or CSS selector
-					type: 'inline'
-				}
-			})
-			// window.location.replace("/thanks.html");
-			setTimeout(function () {
-				// Done Functions
-				th.trigger("reset");
-				// $.magnificPopup.close();
-			}, 4000);
-		});
-		return false;
-	});
-	// /form
-
-
-
-	// / mask for input
-
-
-
-
-
-	// кастомный инпут файл
-
-
-	// или
-	// $(".dropzone").dropzone({
-	//  url: "/file/post",
-	//  addRemoveLinks: true,
-	//      acceptedFiles: 'image/*',
-	//      uploadMultiple: true,
-	//   });
-
-
-
-
-	// $(".wow-wrap").each(function () {
-	// var wowAnim = $(this).find(".s-dop__col," +
-	//                 ".s-pick__col," +
-	//                 ".s-condition__col");
-	// wowAnim.each(function(i){
-
-	// wowAnim.eq(i).attr("data-wow-delay", i*.1*2 + "s");
-
-	//    var wow = new WOW({ mobile: false });
-	//         wow.init();
-
-	// });
-	// });
-
-
-});
